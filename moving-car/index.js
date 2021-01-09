df37416c9478d42187ca86bce59076f078cf7be3
Leonardo Silveira <sombriks@gmail.com>
2021-01-08 23:56:59 -0300

    gc issues

--- moving-car/index.js
+++ moving-car/index.js
@@ -32,9 +32,8 @@ class Car {
     this.right.lFwd.setValue(1);
     this.right.lBwd.setValue(1);
 
-    this.app.get(
-      "/forward",
-      this.step(this.left.lFwd, this.right.lFwd, "moving forward")
+    this.app.get("/forward", (req, res) =>
+      this.step2(this.left.lFwd, this.right.lFwd, req, res, "moving forward")
     );
     this.app.get(
       "/backward",
@@ -61,6 +60,17 @@ class Car {
         console.log(message);
       }, req.query.duration || delay);
     };
+  }
+
+  step2(line1, line2, req, res, message) {
+    line1.setValue(0);
+    line2.setValue(0);
+    setTimeout(() => {
+      line1.setValue(1);
+      line2.setValue(1);
+      res.send("ok");
+      console.log(message);
+    }, req.query.duration || delay);
   }
 
   listen() {
