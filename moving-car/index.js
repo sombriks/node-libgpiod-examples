d610fc5eba9759a31ddb8527fd6480094b8a5289
Leonardo Silveira <sombriks@gmail.com>
2021-01-08 22:55:58 -0300

    sampling with pretty ordinary script to move a car

--- moving-car/index.js
+++ moving-car/index.js
@@ -1,6 +1,8 @@
 const { Chip } = require("node-libgpiod");
 const express = require("express");
 
+const delay = 250;
+
 class Car {
   constructor() {
     this.chip = new Chip(0);
@@ -14,6 +16,49 @@ class Car {
       lineForward: this.chip.getLine(19),
       lineBackward: this.chip.getLine(26),
     };
+
+    this.left.lineForward.requestOutputMode();
+    this.left.lineBackward.requestOutputMode();
+
+    this.right.lineForward.requestOutputMode();
+    this.right.lineBackward.requestOutputMode();
+
+    this.app.get("/forward", (req, res) => {
+      this.left.lineForward.setValue(0);
+      this.right.lineForward.setValue(0);
+      setTimeout(() => {
+        this.left.lineForward.setValue(1);
+        this.right.lineForward.setValue(1);
+        res.send("ok");
+      }, delay);
+    });
+    this.app.get("/backward", (req, res) => {
+      this.left.lineBackward.setValue(0);
+      this.right.lineBackward.setValue(0);
+      setTimeout(() => {
+        this.left.lineBackward.setValue(1);
+        this.right.lineBackward.setValue(1);
+        res.send("ok");
+      }, delay);
+    });
+    this.app.get("/left", (req, res) => {
+      this.left.lineBackward.setValue(0);
+      this.right.lineForward.setValue(0);
+      setTimeout(() => {
+        this.left.lineBackward.setValue(1);
+        this.right.lineForward.setValue(1);
+        res.send("ok");
+      }, delay);
+    });
+    this.app.get("/right", (req, res) => {
+      this.left.lineForward.setValue(0);
+      this.right.lineBackward.setValue(0);
+      setTimeout(() => {
+        this.left.lineForward.setValue(1);
+        this.right.lineBackward.setValue(1);
+        res.send("ok");
+      }, delay);
+    });
   }
 
   listen() {
