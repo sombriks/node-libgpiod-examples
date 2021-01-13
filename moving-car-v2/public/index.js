57eac7f33d4bd0ed17a3b1783f403b28548782b9
Leonardo Silveira <sombriks@gmail.com>
2021-01-12 21:47:25 -0300

    adding keyboard control

--- moving-car-v2/public/index.js
+++ moving-car-v2/public/index.js
@@ -47,3 +47,53 @@ rbwd.addEventListener("mousedown", backwardRight(0));
 rbwd.addEventListener("mouseup", backwardRight(1));
 rbwd.addEventListener("touchstart", backwardRight(0));
 rbwd.addEventListener("touchend", backwardRight(1));
+
+window.addEventListener(
+  "keydown",
+  (e) => {
+    switch (e.key) {
+      case "ArrowLeft":
+        forwardRight(0)();
+        backwardLeft(0)();
+        break;
+      case "ArrowRight":
+        backwardRight(0)();
+        forwardLeft(0)();
+        break;
+      case "ArrowUp":
+        forwardLeft(0)();
+        forwardRight(0)();
+        break;
+      case "ArrowDown":
+        backwardLeft(0)();
+        backwardRight(0)();
+        break;
+    }
+  },
+  true
+);
+
+window.addEventListener(
+  "keyup",
+  (e) => {
+    switch (e.key) {
+      case "ArrowLeft":
+        forwardRight(1)();
+        backwardLeft(1)();
+        break;
+      case "ArrowRight":
+        backwardRight(1)();
+        forwardLeft(1)();
+        break;
+      case "ArrowUp":
+        forwardLeft(1)();
+        forwardRight(1)();
+        break;
+      case "ArrowDown":
+        backwardLeft(1)();
+        backwardRight(1)();
+        break;
+    }
+  },
+  true
+);
