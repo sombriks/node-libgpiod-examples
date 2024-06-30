e6093dd07ea3952e543f0539271d52a2dda3089c
Leonardo Silveira <sombriks@gmail.com>
2024-06-30 13:19:23 -0300

    investigating mysql issue

--- database-read-pin-write/index.js
+++ database-read-pin-write/index.js
@@ -5,21 +5,23 @@ const conn = mysql.createConnection({
   database: "gpiodb",
   user: "gpiousr",
   password: "gpiopwd",
-  host: "thanatos.local",
-  //host: "localhost",
+  host: process.env.MYSQL_HOST ?? "localhost",
+  //host: "thanatos.local",
 })
 
 let current = 0
-const ops = [()=> console.log("no operation")]
+const ops = [() => console.log("no operation")]
 const delay = 3000
 let timerId = null
 
+// see init.sql for schema details
 conn.query("select * from pins", (err, result) => {
 
   if (err) throw err
 
   result.forEach(p => {
     ops.push(() => {
+      // enqueue operations based on mysql results
       console.log(`pin ${JSON.stringify(p)}`)
       const pin = gpiod.Pin(p.pin)
       pin.requestOutputMode()
@@ -29,11 +31,12 @@ conn.query("select * from pins", (err, result) => {
     })
   })
   ops.push(() => {
+    // ending operation
     clearInterval(timerId)
     conn.destroy()
   })
   current++ // play 
-  
+
 })
 
 timerId = setInterval(() => ops[current](), delay)
--- database-read-pin-write/init.sql
+++ database-read-pin-write/init.sql
@@ -2,13 +2,18 @@ create table pins(
   id integer primary key auto_increment,
   pin integer not null,
   val integer not null,
-  created timestamp not null default current_timestamp 
+  created timestamp not null default current_timestamp
 );
-
-insert into pins(pin,val) 
-  values  (22,1),
-          (22,0),
-          (22,1),
-          (22,0),
-          (27,1),
-          (27,0); 
+insert into pins(pin, val)
+values (17, 1),
+  (22, 1),
+  (27, 1),
+  (17, 0),
+  (22, 0),
+  (27, 0),
+  (17, 1),
+  (17, 0),
+  (22, 1),
+  (22, 0),
+  (27, 1),
+  (27, 0);
