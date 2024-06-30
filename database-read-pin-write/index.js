2eadf48feb08d06e52de3253d938bb33a4b1ca6a
Leonardo Silveira <sombriks@gmail.com>
2024-06-30 13:14:04 -0300

    seems to be working fine on latest Raspbian...

--- database-read-pin-write/index.js
+++ database-read-pin-write/index.js
@@ -2,23 +2,38 @@ import gpiod from "node-libgpiod"
 import mysql from "mysql2"
 
 const conn = mysql.createConnection({
-  database: 'gpiodb',
-  user: 'gpiousr',
-  password: 'gpiopwd',
-  host: 'localhost',
-});
+  database: "gpiodb",
+  user: "gpiousr",
+  password: "gpiopwd",
+  host: "thanatos.local",
+  //host: "localhost",
+})
+
+let current = 0
+const ops = [()=> console.log("no operation")]
+const delay = 3000
+let timerId = null
 
 conn.query("select * from pins", (err, result) => {
 
   if (err) throw err
 
   result.forEach(p => {
-    console.log(`pin ${JSON.stringify(p)}`)
-    const pin = gpiod.Pin(p.pin)
-    pin.requestOutputMode()
-    pin.setValue(p.val)
-    pin.release()
+    ops.push(() => {
+      console.log(`pin ${JSON.stringify(p)}`)
+      const pin = gpiod.Pin(p.pin)
+      pin.requestOutputMode()
+      pin.setValue(p.val)
+      pin.release()
+      current++
+    })
   })
-  conn.close();
+  ops.push(() => {
+    clearInterval(timerId)
+    conn.destroy()
+  })
+  current++ // play 
+  
 })
 
+timerId = setInterval(() => ops[current](), delay)
