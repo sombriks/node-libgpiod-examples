bc2fd1c9a71df54fc08a68b7d0f047624fec2201
Leonardo Silveira <sombriks@gmail.com>
2026-02-16 22:10:43 +0000

    works on device ui

--- device-ui/index.js
+++ device-ui/index.js
@@ -1,4 +1,20 @@
-const fastify = require('fastify')({
-  logger: true
+// index.js
+const express = require('express')
+const gpio = require('node-libgpiod')
+
+const app = express()
+app.set('view engine', 'pug')
+
+// static assets
+app.use('/bulma', express.static('node_modules/bulma/css'))
+app.use('/htmx.org', express.static('node_modules/htmx.org/dist'))
+
+// ui routes
+app.get('/', (req, res) => {
+  const names = gpio.chipNames
+  res.render('pages/index', { names })
 })
 
+app.listen(3000, () => {
+  console.log('Web Blinker app listening on port 3000')
+})
