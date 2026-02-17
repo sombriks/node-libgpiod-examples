688dcb5c1a2c387ade0f76dab8e5d71912ceee03
Leonardo Silveira <sombriks@gmail.com>
2026-02-17 19:28:42 +0000

    device ui can on and off pins now

--- device-ui/index.js
+++ device-ui/index.js
@@ -59,6 +59,24 @@ app.put('/chip/:chipName/line/:offset/request-output', (req, res) => {
   res.render('partials/pin', { result })
 })
 
+app.put('/chip/:chipName/line/:offset/set/:value', (req, res) => {
+  const chip = new gpio.Chip(req.params.chipName)
+  const line = chip.getLine(parseInt(req.params.offset))
+  line.requestOutputMode()
+  line.setValue(parseInt(req.params.value))
+  const result = {
+    chipName: chip.name,
+    offset: line.offset,
+    name: line.name,
+    value: line.value,
+    consumer: line.consumer,
+    direction: line.direction,
+    activeState: line.activeState
+  }
+  line.release()
+  res.render('partials/pin', { result })
+})
+
 // open to busibess
 app.listen(3000, () => {
   console.log('Web Blinker app listening on port 3000')
